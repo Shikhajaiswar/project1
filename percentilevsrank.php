425f2683c840fc0144bf46a0192a03ad78afd583
Pawan Kumar <97592776+Pawan20031@users.noreply.github.com>
2023-01-19 23:50:56 +0530

    Update percentilevsrank.php

--- percentilevsrank.php
+++ percentilevsrank.php
@@ -170,6 +170,87 @@
         </div>
     </div>
     </div>
+    <div class="row mt-3 mb-5">
+
+        <div class="list-group col-md-6 offset-md-3 shadow me-5  p-4 animate__animated animate__bounceInLeft ">
+            <h4>FAQs on JEE Main Marks Vs Percentile Vs Rank</h4>
+            <br>
+            <a style="text-decoration: none;" data-bs-toggle="collapse" href="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
+                <button type="button" class="list-group-item list-group-item-action " aria-current="true">
+                    <div class="row">
+                        <div class="col-auto me-auto"> 1. How to calculate JEE Main percentile and rank?</div>
+                        <div class="col-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                            </svg></div>
+                    </div>
+
+                </button>
+            </a>
+
+            <div class="collapse text-center bg-warning" id="collapseExample1">
+                <div class="card card-body text-center bg-warning">
+                    NTA makes use of a formula to arrive at the percentile score of applicants with respect to the actual scores. <br> Percentiles determine normalized rankings because the exam is carried out in 2 sessions.
+                    <br> The formula required to calculate rank from the percentile is:
+                    <br>
+                    ((100 – P)/100)×N + 1
+                    <br>
+                    Here,
+                    <br>
+                    NTA Score: P
+                    <br>
+                    Total number of students: N
+                </div>
+            </div>
+            <a style="text-decoration: none;" data-bs-toggle="collapse" href="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
+                <button type="button" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-auto me-auto">2. What will be the qualifying percentile in JEE Main 2022? </div>
+                        <div class="col-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                            </svg></div>
+                    </div>
+
+                </button>
+            </a>
+            <div class="collapse" id="collapseExample2">
+                <div class="card card-body bg-warning">
+                    NTA will release on 6 July the official results once the exams are conducted successfully. 75 percent for the general category and a minimum of 65 percent for SC/SC/PWD category candidates is considered as the qualifying percentile in JEE Main 2022.
+                </div>
+            </div>
+            <a style="text-decoration: none;" data-bs-toggle="collapse" href="#collapseExample3" aria-expanded="false" aria-controls="collapseExample">
+                <button type="button" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-auto me-auto">3. If a candidate scored 280 marks, what will be the JEE Main percentile score? </div>
+                        <div class="col-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                            </svg></div>
+                    </div>
+
+                </button></a>
+            <div class="collapse" id="collapseExample3">
+                <div class="card card-body bg-warning">
+                    According to the NTA result trends a candidate with 280 marks, the percentile might be close to 99. One can score a place in IIT and NIT.
+                </div>
+            </div>
+            <a style="text-decoration: none;" data-bs-toggle="collapse" href="#collapseExample4" aria-expanded="false" aria-controls="collapseExample">
+                <button type="button" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-auto me-auto">4. Will the JEE Main 2022 result will be declared offline? </div>
+                        <div class="col-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                            </svg></div>
+                    </div>
+
+                </button>
+            </a>
+            <div class="collapse" id="collapseExample4">
+                <div class="card card-body bg-warning">
+                    No, the JEE Main 2022 will be declared online.
+                </div>
+            </div>
+
+        </div>
+    </div>
     <?php
     include 'footer.php';
     ?>
