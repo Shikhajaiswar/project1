a2fe9d642bafb83a0fefdb749a15f745624a4738
Pawan Kumar <97592776+Pawan20031@users.noreply.github.com>
2023-01-19 22:41:04 +0530

    Add files via upload

---
+++ percentilevsrank.php
@@ -0,0 +1,179 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style>
+        .bg {
+            background: purple;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="row">
+        <div class="col-md-6 offset-md-3 p border-1 shadow mt-5 mb-4 p-5 animate__animated animate__bounceInLeft">
+            <div class="text-center">
+                <h3>JEE Main Percentile Vs Rank Analysis</h3>
+                <br>
+                <p>Apart from assessing the percentile, it is also crucial for students to determine what rank will be awarded to them against their percentile. Furthermore, along with a comprehensive understanding of JEE Main marks vs percentile, students need to know which rank is awarded to each percentile. A general assessment of the rank with the percentile is shown here in a tabular format.</p>
+                <table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">JEE Main Percentile</th>
+                            <th scope="col">JEE Main Rank</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">100 - 99.99989145</th>
+                            <td>1 - 20</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.994681 - 99.997394</th>
+                            <td>80 - 24</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.990990 - 99.994029</th>
+                            <td>83 - 55</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.977205 - 99.988819</th>
+                            <td>210 - 85</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.960163 - 99.975034</th>
+                            <td>367 - 215</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.934980 - 99.956364</th>
+                            <td>599 - 375</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.901113 - 99.928901</th>
+                            <td>911 - 610</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.851616 - 99.893732</th>
+                            <td>1367 - 920</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.795063 - 99.845212</th>
+                            <td>1888 - 1375</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.710831 - 99.782472</th>
+                            <td>2664 - 1900</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.597399 - 99.688579</th>
+                            <td>3710 - 2700</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.456939 - 99.573193</th>
+                            <td>5003- 3800</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.272084 - 99.431214</th>
+                            <td>6706 - 5100</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">99.028614 - 99.239737</th>
+                            <td>8949 - 6800</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">98.732389 - 98.990296</th>
+                            <td>11678 - 9000</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">98.317414 - 98.666935</th>
+                            <td>15501 - 11800</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">97.811260 - 98.254132</th>
+                            <td>20164 - 15700</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">97.142937 - 97.685672</th>
+                            <td>26321 - 20500</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">96.204550 - 96.978272</th>
+                            <td>34966 - 26500</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">94.998594 - 96.064850</th>
+                            <td>46076 - 35000</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">93.471231 - 94.749479</th>
+                            <td>60147 - 46500</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">91.072128 - 93.152971</th>
+                            <td>82249 - 61000</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">87.512225 - 90.702200</th>
+                            <td>115045 - 83000</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">82.016062 - 86.907944</th>
+                            <td>165679 - 11700</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">73.287808 - 80.982153</th>
+                            <td>246089 - 166000</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">58.151490 - 71.302052</th>
+                            <td>385534 - 26438</td>
+
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    </div>
+    <?php
+    include 'footer.php';
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ rankvsmarks.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style>
+        .bg{
+            background: purple;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="row">
+        <div class="col-md-6 offset-md-3 p border-1 shadow mt-5 mb-4 p-5 animate__animated animate__bounceInLeft">
+            <div class="text-center">
+                <h3>JEE Main Rank Vs Marks Analysis</h3>
+                <br>
+                <p>The table below provides a rank vs marks prediction. It also helps students understand the JEE Main rank-wise college prediction. Students who will secure ranks within 1 to 2000 are most likely to score a place in top IITs, whereas students who fall under the rank of 2000 to 7000 are most likely to score a place in top NITs and IITs. The table contains rank vs marks prediction which can be used for college prediction, so one must strive to score a good rank to ensure their place in their dream college.</p>
+                <table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">JEE Main Marks (out of 300)</th>
+                            <th scope="col">JEE Main Rank</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">288-294</th>
+                            <td>20-11</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">281-285</th>
+                            <td>44-22</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">269- 280</th>
+                            <td>108-66</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">249- 267</th>
+                            <td>520-106</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">231-247</th>
+                            <td>1382-543</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">214-229</th>
+                            <td>2796-1420</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">200-212</th>
+                            <td>4665-2861</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">189-199</th>
+                            <td>6664- 4830</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">175-188</th>
+                            <td>10746-7152</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">160-174</th>
+                            <td>16163-11018</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">149-159</th>
+                            <td>21145-16495</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">132-148</th>
+                            <td>32826-22238</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">120-131</th>
+                            <td>43174-33636</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">110-119</th>
+                            <td>54293-44115</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">102-109</th>
+                            <td>65758-55269</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">95-101</th>
+                            <td>76260-66999</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">89-94</th>
+                            <td>87219-78111</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">79-88</th>
+                            <td>109329-90144</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">66-87</th>
+                            <td>169540-92300</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">44-63</th>
+                            <td>326513-173230</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">1-42</th>
+                            <td>1025019-3340780</td>
+
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    </div>
+    <?php 
+    include 'footer.php';
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
